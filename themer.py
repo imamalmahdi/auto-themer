8af600460184af159d7b561aec88922dd29e0b29
demonicshady <mahdihossain987@gmail.com>
2020-04-16 20:05:16 +0600

    Now, the script knows the time of the day!

--- themer.py
+++ themer.py
@@ -1,20 +1,27 @@
 import os
 import subprocess
+import shutil
+from pathlib import Path
+from datetime import datetime
 
+current_dir = Path(__file__).parent.absolute()
 
 class Widget:
     def __init__(self, name):
         docs = r"C:\Users\mahdi\OneDrive\Documents\Rainmeter\Skins"
         self.name = name
-        self.main = docs + '\\' + name.split("\\")[0] + r"\@Resources\Variables.inc"
-        self.mid = self.main + ".a"
-        self.bak = self.main + ".bak"
+        self.absolute = name.split("\\")[0]
+        self.main = docs + '\\' + self.absolute + r"\@Resources\Variables.inc"
     
     def switch(self):
-        os.rename(self.main, self.mid)
-        os.rename(self.bak, self.main)
-        os.rename(self.mid, self.bak)
+        time = datetime.now().timetuple()
+        if time.tm_hour >= 17 or time.tm_hour < 5:
+            file = f"{current_dir}\\{self.absolute}\\dark.inc"
+        else:
+            file = f"{current_dir}\\{self.absolute}\\light.inc"
 
+        os.remove(self.main)
+        shutil.copy(file, self.main)
         path = r'"C:\Program Files\Rainmeter\Rainmeter.exe" !Refresh '
         command = path + f'"{self.name}"'
         subprocess.call(command)
@@ -25,5 +32,3 @@ lyrics_widget.switch()
 
 info_widget = Widget("Lumiero\\Song Info")
 info_widget.switch()
-
-
